ea2031ed09516d3b02a3a1f0efbb8a6b2ab9a426
Steve Jackson <steve.jackson@leandogsoftware.com>
2013-07-28 08:44:50 -0400

    Handle one bottle of beer

--- ruby/beer-song/beer.rb
+++ ruby/beer-song/beer.rb
@@ -1,7 +1,41 @@
 class Beer
-  def verse(arg)
-%Q{8 bottles of beer on the wall, 8 bottles of beer.
-Take one down and pass it around, 7 bottles of beer on the wall.
-}
+
+  def verse(num)
+    line1(num) << line2(num-1)
+  end
+
+  def line1(num)
+    "#{bottles_on_wall(num)}, #{bottles_of_beer(num)}.\n"
+  end
+
+  def line2(num)
+    "#{take_bottle(num)} and pass it around, #{bottles_on_wall(num)}.\n"
+  end
+
+  def bottles_on_wall(num)
+    "#{bottles(num)} of beer on the wall"
+  end
+
+  def bottles_of_beer(num)
+    "#{bottles(num)} of beer"
+  end
+
+  def take_bottle(num)
+    if(num > 1)
+      'Take one down'
+    else
+      'Take it down'
+    end
+  end
+
+
+  def bottles(num)
+    if(num == 0)
+      "no more bottles"
+    elsif(num == 1)
+      "#{num} bottle"
+    else
+      "#{num} bottles"
+    end
   end
 end
