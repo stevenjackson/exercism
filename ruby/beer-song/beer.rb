5342dfb0812233bf827019e2650c2c0e7768c070
Steve Jackson <steve.jackson@leandogsoftware.com>
2013-07-30 07:06:35 -0400

    More OO beer song

--- ruby/beer-song/beer.rb
+++ ruby/beer-song/beer.rb
@@ -1,96 +1,72 @@
 class Beer
 
-  def verse(num)
-    Verse.new(Wall.new(num)).to_s
+  def verse(number)
+    song[number].sing(number)
   end
 
   def sing(start_at, end_at=0)
     verses(start_at, end_at).join("\n") << "\n"
   end
 
-  def verses(start_at, end_at)
-    start_at.downto(end_at).map { |num| verse(num) }
+  private
+
+  def song
+    song = Hash.new(Verse.new)
+    song[2] = AntepenultimateVerse.new
+    song[1] = PenultimateVerse.new
+    song[0] = LastVerse.new
+    song
   end
 
-  class Wall
-    attr_accessor :bottles
-
-    def initialize(bottles)
-      @bottles = bottles
-    end
-
-    def take
-      self.bottles -= 1
-    end
-
-    def buy_more
-      self.bottles = 99
-    end
-
-    def bottles?
-      bottles >= 1
-    end
-
+  def verses(start_at, end_at)
+    start_at.downto(end_at).map { |number| verse(number) }
   end
 
   class Verse
-    attr_reader :wall
+    attr_reader :verse_number
 
-    def initialize(wall)
-      @wall = wall
+    def sing(verse_number)
+      @verse_number = verse_number
+      phrases.join
     end
 
-    def to_s
-      [line1, line2].map(&:capitalize).join("\n") << "\n"
-    end
-
-    def line1
-      "#{bottles_on_wall}, #{bottles_of_beer}."
-    end
-
-    def line2
-      if wall.bottles?
-        wall.take
-        take_and_pass
-      else
-        wall.buy_more
-        buy_some_more
-      end
-    end
-
-    def take_and_pass
-      "#{take_one} and pass it around, #{bottles_on_wall}."
-    end
-
-    def take_one
-      if(wall.bottles?)
-        'Take one down'
-      else
-        'Take it down'
-      end
-    end
-
-    def buy_some_more
-      "Go to the store and buy some more, #{bottles_on_wall}."
-    end
-
-    def bottles_on_wall
-      "#{bottles_of_beer} on the wall"
-    end
-
-    def bottles_of_beer
-      "#{bottles} of beer"
+    def phrases
+      ["#{bottles} bottles of beer on the wall, ",
+       "#{bottles} bottles of beer.\n",
+       "Take one down and pass it around, ",
+       "#{bottles - 1} bottles of beer on the wall.\n"]
     end
 
     def bottles
-      if(wall.bottles == 0)
-        "no more bottles"
-      elsif(wall.bottles == 1)
-        "#{wall.bottles} bottle"
-      else
-        "#{wall.bottles} bottles"
-      end
+      verse_number
     end
-
   end
+
+  class AntepenultimateVerse < Verse
+    def phrases
+      ["2 bottles of beer on the wall, ",
+       "2 bottles of beer.\n",
+       "Take one down and pass it around, ",
+       "1 bottle of beer on the wall.\n"]
+    end
+  end
+
+  class PenultimateVerse < Verse
+    def phrases
+      ["1 bottle of beer on the wall, ",
+       "1 bottle of beer.\n",
+       "Take it down and pass it around, ",
+       "no more bottles of beer on the wall.\n"]
+    end
+  end
+
+  class LastVerse < Verse
+    def phrases
+      ["No more bottles of beer on the wall, ",
+       "no more bottles of beer.\n", 
+       "Go to the store and buy some more, ",
+       "99 bottles of beer on the wall.\n"]
+    end
+  end
+
 end
