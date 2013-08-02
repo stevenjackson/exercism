c1b498363d593740dcc3c6ada5cd77be359f54a5
Steve Jackson <steve.jackson@leandogsoftware.com>
2013-08-02 13:45:23 -0400

    First cut at nucelotide counting

--- ruby/nucleotide-count/dna.rb
+++ ruby/nucleotide-count/dna.rb
@@ -1,6 +1,36 @@
-class DNA < String
+module Nucleotide
+  ADENOSINE = 'A'
+  CYTIDINE  = 'C'
+  GUANOSINE = 'G'
+  THYMIDINE = 'T'
+  URACIL    = 'U'
+
+  def self.nucleotide?(string)
+    constants.any? { |const| const_get(const) == string }
+  end
+end
+
+class DNA
+  include Nucleotide
+
+  attr_reader :sequence
+
+  def initialize(sequence)
+    @sequence = sequence
+  end
+
   def count(nucleotide)
-    0
+    raise ArgumentError unless Nucleotide.nucleotide? nucleotide
+    sequence.count(nucleotide)
+  end
+
+  def nucleotide_counts
+    @nucleotide_counts ||= {
+      ADENOSINE => count(ADENOSINE),
+      THYMIDINE => count(THYMIDINE),
+      CYTIDINE => count(CYTIDINE),
+      GUANOSINE => count(GUANOSINE)
+    }
   end
 
 end
