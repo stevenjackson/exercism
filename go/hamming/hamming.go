174bff179f56e11f02f97f27be557ecc84660bac
Steve Jackson <steve@testdouble.com>
2018-10-03 10:40:46 -0400

    [go] lint hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -2,13 +2,14 @@ package hamming
 
 import "fmt"
 
+// Distance returns the Hamming distance between two strands
 func Distance(a, b string) (int, error) {
 	if len(a) != len(b) {
 		return -1, fmt.Errorf("strands must be same length")
 	}
 
 	distance := 0
-	for i, _ := range a {
+	for i := range a {
 		if b[i] != a[i] {
 			distance++
 		}
